90dafabbadf88b03f8c47067ce729c8432332f4f
Jon Smith <jon.smith@selectiveanalytics.com>
2019-11-30 11:21:38 +0000

    Wehn to delta calc of new count/average reviews

--- Infrastructure/EventHandlers/ReviewAddedHandler.cs
+++ Infrastructure/EventHandlers/ReviewAddedHandler.cs
@@ -2,10 +2,7 @@
 // Licensed under MIT license. See License.txt in the project root for license information.
 
 using System;
-using System.Linq;
 using DataLayerEvents.DomainEvents;
-using DataLayerEvents.EfClasses;
-using DataLayerEvents.EfCode;
 using GenericEventRunner.ForEntities;
 using GenericEventRunner.ForHandlers;
 using StatusGeneric;
@@ -14,47 +11,13 @@ namespace Infrastructure.EventHandlers
 {
     public class ReviewAddedHandler : IBeforeSaveEventHandler<BookReviewAddedEvent>
     {
-        private readonly SqlEventsDbContext _context;
-
-        public ReviewAddedHandler(SqlEventsDbContext context)
-        {
-            _context = context;
-        }
-
         public IStatusGeneric Handle(EntityEvents callingEntity, BookReviewAddedEvent domainEvent)
         {
-            if (domainEvent.Book.Reviews != null)
-            {
-                //The reviews collection is filled, either because the book was created, or it was loaded with .Include(x => x.Reviews)
-                var numReviews = domainEvent.Book.Reviews.Count();
-                var reviewsAverageVotes = domainEvent.Book.Reviews.ToList().Average(x => (double)x.NumStars);
-                domainEvent.UpdateReviewCachedValues(numReviews, reviewsAverageVotes);
-
-                return null;
-            }
-
-            if (true)
-            {
-                //This is the slow but sure way
-
-                var currentReviewNumStars = _context.Set<ReviewWithEvents>()
-                    .Where(x => x.BookId == domainEvent.Book.BookId)
-                    .Select(x => x.NumStars).ToList();
-
-                currentReviewNumStars.Add(domainEvent.NumStars);
-                var numReviews = currentReviewNumStars.Count;
-                var reviewsAverageVotes = currentReviewNumStars.Average();
-
-                domainEvent.UpdateReviewCachedValues(numReviews, reviewsAverageVotes);
-            }
-            else
-            {
-                //Here is the fast (delta) version of the update. Doesn't need access to the database
-                var totalStars = Math.Round(domainEvent.Book.ReviewsAverageVotes * domainEvent.Book.ReviewsCount) +
-                                 domainEvent.NumStars;
-                var numReviews = domainEvent.Book.ReviewsCount + 1;
-                domainEvent.UpdateReviewCachedValues(numReviews, totalStars/numReviews);
-            }
+            //Here is the fast (delta) version of the update. Doesn't need access to the database
+            var totalStars = Math.Round(domainEvent.Book.ReviewsAverageVotes * domainEvent.Book.ReviewsCount) +
+                             domainEvent.NumStars;
+            var numReviews = domainEvent.Book.ReviewsCount + 1;
+            domainEvent.UpdateReviewCachedValues(numReviews, totalStars / numReviews);
 
             return null;
         }
--- Infrastructure/EventHandlers/ReviewRemovedHandler.cs
+++ Infrastructure/EventHandlers/ReviewRemovedHandler.cs
@@ -2,10 +2,7 @@
 // Licensed under MIT license. See License.txt in the project root for license information.
 
 using System;
-using System.Linq;
 using DataLayerEvents.DomainEvents;
-using DataLayerEvents.EfClasses;
-using DataLayerEvents.EfCode;
 using GenericEventRunner.ForEntities;
 using GenericEventRunner.ForHandlers;
 using StatusGeneric;
@@ -14,45 +11,13 @@ namespace Infrastructure.EventHandlers
 {
     public class ReviewRemovedHandler : IBeforeSaveEventHandler<BookReviewRemovedEvent>
     {
-        private readonly SqlEventsDbContext _context;
-
-        public ReviewRemovedHandler(SqlEventsDbContext context)
-        {
-            _context = context;
-        }
-
         public IStatusGeneric Handle(EntityEvents callingEntity, BookReviewRemovedEvent domainEvent)
         {
-            if (domainEvent.Book.Reviews != null)
-            {
-                //The reviews collection is filled, either because the book was created, or it was loaded with .Include(x => x.Reviews)
-                var numReviews = domainEvent.Book.Reviews.Count();
-                var reviewsAverageVotes = domainEvent.Book.Reviews.ToList().Average(x => (double)x.NumStars);
-                domainEvent.UpdateReviewCachedValues(numReviews, reviewsAverageVotes);
-
-                return null;
-            }
-
-            if (true)
-            {
-                //This is the slow but sure way
-                var reviewsNumStarsWithoutRemoved = _context.Set<ReviewWithEvents>()
-                    .Where(x => x.BookId == domainEvent.Book.BookId && x.ReviewId != domainEvent.ReviewRemoved.ReviewId)
-                    .Select(x => x.NumStars).ToList();
-
-                var numReviews = reviewsNumStarsWithoutRemoved.Count;
-                var reviewsAverageVotes = reviewsNumStarsWithoutRemoved.Average();
-
-                domainEvent.UpdateReviewCachedValues(numReviews, reviewsAverageVotes);
-            }
-            else
-            {
-                //Here is the fast (delta) version of the update. Doesn't need access to the database
-                var numReviews = domainEvent.Book.ReviewsCount - 1;
-                var totalStars = Math.Round(domainEvent.Book.ReviewsAverageVotes * domainEvent.Book.ReviewsCount)
-                                 - domainEvent.ReviewRemoved.NumStars;
-                domainEvent.UpdateReviewCachedValues(numReviews, totalStars/numReviews);
-            }
+            //Here is the fast (delta) version of the update. Doesn't need access to the database
+            var numReviews = domainEvent.Book.ReviewsCount - 1;
+            var totalStars = Math.Round(domainEvent.Book.ReviewsAverageVotes * domainEvent.Book.ReviewsCount)
+                             - domainEvent.ReviewRemoved.NumStars;
+            domainEvent.UpdateReviewCachedValues(numReviews, totalStars / numReviews);
 
             return null;
         }

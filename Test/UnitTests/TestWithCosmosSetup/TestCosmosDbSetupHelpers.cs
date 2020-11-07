c7bfecb66561eefbf063e0bd47ded55a554f0d46
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-11-07 14:37:56 +0000

    Added more tests

--- Test/UnitTests/TestWithCosmosSetup/TestCosmosDbSetupHelpers.cs
+++ Test/UnitTests/TestWithCosmosSetup/TestCosmosDbSetupHelpers.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) 2020 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
 // Licensed under MIT license. See License.txt in the project root for license information.
 
+using System.Linq;
 using System.Threading.Tasks;
 using DataLayer.EfCode;
 using Microsoft.EntityFrameworkCore;
 using Test.Helpers;
+using TestSupport.EfHelpers;
 using Xunit;
 using Xunit.Abstractions;
 using Xunit.Extensions.AssertExtensions;
@@ -88,6 +90,85 @@ namespace Test.UnitTests.TestWithCosmosSetup
 
                 //VERIFY
                 (await context.Books.CountAsync()).ShouldBeInRange(1, 10000);
+            }
+        }
+
+        [Fact]
+        public async Task TestAzureUnitTestCosmosWriteManyOk()
+        {
+            //SETUP
+            var options = "AzureUnitTest".GetCosmosEfCoreOptions<NoSqlDbContext>(GetType().Name);
+
+            using (var context = new NoSqlDbContext(options))
+            {
+                await context.Database.EnsureCreatedAsync();
+
+                //ATTEMPT
+                await TimeEfCoreWrite(10, context);
+                await TimeEfCoreWrite(100, context);
+                await TimeEfCoreWrite(100, context);
+                await TimeEfCoreWrite(10, context);
+                await TimeEfCoreWrite(100, context);
+
+                //VERIFY
+                (await context.Books.CountAsync()).ShouldBeInRange(1, 10000);
+            }
+        }
+
+        private async Task TimeEfCoreWrite(int totalToWrite, NoSqlDbContext context)
+        {
+            using (new TimeThings(_output, $"Wrote out {totalToWrite} via EF Core"))
+            {
+                var books = NoSqlTestData.CreateDummyBooks(totalToWrite);
+                context.AddRange(books);
+                await context.SaveChangesAsync();
+            }
+        }
+
+
+        [Fact]
+        public async Task TestCosmosDbReadViaEfCoreOk()
+        {
+            //SETUP
+            var options = "AzureUnitTest".GetCosmosEfCoreOptions<NoSqlDbContext>(GetType().Name);
+
+            using var noSqlContext = new NoSqlDbContext(options);
+
+            //ATTEMPT
+            await TimeReadBooksEfCore(10, noSqlContext);
+            await TimeReadBooksEfCore(100, noSqlContext);
+            await TimeReadBooksEfCore(100, noSqlContext);
+            await TimeReadBooksEfCore(10, noSqlContext);
+            await TimeReadBooksEfCore(100, noSqlContext);
+        }
+
+        private async Task TimeReadBooksEfCore(int numRead, NoSqlDbContext noSqlContext)
+        {
+            using (new TimeThings(_output, $"read {numRead} books via EF Core"))
+            {
+                var books = await noSqlContext.Books.AsNoTracking().Take(numRead).ToListAsync();
+            }
+        }
+
+
+        [Fact]
+        public async Task TestAzureUnitTestCosmosDbOptionsDeleteDatabaseAndRecreateOk()
+        {
+            //SETUP
+            var options = "AzureUnitTest".GetCosmosEfCoreOptions<NoSqlDbContext>(GetType().Name +"_different");
+
+            using (var context = new NoSqlDbContext(options))
+            {
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.EnsureCreatedAsync();
+
+                //ATTEMPT
+                var book = NoSqlTestData.CreateDummyNoSqlBook();
+                context.Add(book);
+                await context.SaveChangesAsync();
+
+                //VERIFY
+                (await context.Books.CountAsync()).ShouldEqual(1);
             }
         }
     }
